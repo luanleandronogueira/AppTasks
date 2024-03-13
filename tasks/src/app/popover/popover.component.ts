6a9f41d22b925a0cdba4db2e259ab9a142411009
Luan Nogueira <luannogueira093@gmail.com>
2024-03-13 13:40:39 -0300

    Criação e ajuste da Popover

--- tasks/src/app/popover/popover.component.ts
+++ tasks/src/app/popover/popover.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-popover',
   templateUrl: './popover.component.html',
   styleUrls: ['./popover.component.scss'],
 })
-export class PopoverComponent  implements OnInit {
+export class PopoverComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
 
 }
+
+@NgModule({
+  imports: [
+    IonicModule
+  ],
+  declarations: [
+    PopoverComponent
+  ]
+})
+export class PopoverModule {}
